1fd217be7aeb911419d407c39019060dadfc0e1e
jjerry-k <jaeyeol2931@gmail.com>
2024-05-10 23:36:22 +0900

    Update

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
 import random
+import requests
 from datetime import datetime
 from pytz import timezone
-from github import Github, Auth
+
 
 if __name__ == "__main__":
-    access_token = os.environ['GITHUB_TOKEN']
-    repository_name = "github-actions"
-    seoul_timezone = timezone('Asia/Seoul')
+    access_token = os.environ["GITHUB_TOKEN"]
+    repository_name = os.environ["GITHUB_REPO"]
+    seoul_timezone = timezone("Asia/Seoul")
     today = datetime.now(seoul_timezone)
     today_date = today.strftime("%Y년 %m월 %d일")
-    with Github(Auth.Token(access_token)) as g:
-        repo = g.get_user().get_repo(repository_name)
-    repo.create_issue(title=f"Random Number (Using Python): {int(random.random()*100)}", body="")
+    headers = {
+            "Authorization": f"token {access_token}",
+            "Accept": "application/vnd.github.v3+json",
+            "Content-Type": "application/x-www-form-urlencoded",
+            }
+
+    data = f'{{"title": "Random Number (Using Python): {random.randint(1, 1000)}"}}'
+
+    response = requests.post(f"https://api.github.com/repos/{repository_name}/issues", headers=headers, data=data)
     print("Uploaded Random Number to Github Issue!")
