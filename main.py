aca7dcce4fa5d4af4caa4673c9ed14c788b920e1
jjerry-k <jaeyeol2931@gmail.com>
2024-05-10 23:09:08 +0900

    Update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import random
 from datetime import datetime
 from pytz import timezone
-from github import Github
+from github import Github, Auth
 
 if __name__ == "__main__":
     access_token = os.environ['GITHUB_TOKEN']
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     seoul_timezone = timezone('Asia/Seoul')
     today = datetime.now(seoul_timezone)
     today_date = today.strftime("%Y년 %m월 %d일")
-    
-    with Github(access_token) as g:
+    with Github(Auth.Token(access_token)) as g:
         repo = g.get_user().get_repo(repository_name)
-    repo.create_issue(title=today_date, body=random.random())
+    repo.create_issue(title=f"Random Number (Using Python): {int(random.random()*100)}", body="")
     print("Uploaded Random Number to Github Issue!")
